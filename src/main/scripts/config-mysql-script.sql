7f83c0ec8782911648c760c9e04779b36b0f18ab
Lukas Molcan <lukas.molcan@gmail.com>
2020-10-20 18:06:41 +0200

    Added config.yml and script

---
+++ src/main/scripts/config-mysql-script.sql
@@ -0,0 +1,15 @@
+CREATE SCHEMA sfg_dev;
+CREATE SCHEMA sfg_prod;
+
+CREATE USER 'sfg_dev_user'@'localhost' IDENTIFIED BY 'sfg_dev_user';
+CREATE USER 'sfg_prod_user'@'localhost' IDENTIFIED BY 'sfg_prod_user';
+
+GRANT SELECT ON sfg_dev.* to 'sfg_dev_user'@'localhost';
+GRANT INSERT ON sfg_dev.* to 'sfg_dev_user'@'localhost';
+GRANT DELETE ON sfg_dev.* to 'sfg_dev_user'@'localhost';
+GRANT UPDATE ON sfg_dev.* to 'sfg_dev_user'@'localhost';
+
+GRANT SELECT ON sfg_prod.* to 'sfg_prod_user'@'localhost';
+GRANT INSERT ON sfg_prod.* to 'sfg_prod_user'@'localhost';
+GRANT DELETE ON sfg_prod.* to 'sfg_prod_user'@'localhost';
+GRANT UPDATE ON sfg_prod.* to 'sfg_prod_user'@'localhost';
